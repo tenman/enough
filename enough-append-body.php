98369408f1447f5dde09e0bc1fc1e731219a59cb
tenman <a.tenman@gmail.com>
2014-04-30 15:26:39 +0900

    new file:   enough-append-body.php

---
+++ enough-append-body.php
@@ -0,0 +1,21 @@
+<div class="enough-status-bar">
+ 
+ <p class="move-to-top""><a href="#">top</a></p>  
+ <div style="margin-left:100px; position:relative;"> 
+    <div class="enough-monthly-archive-prev-next-navigation">
+     <?php enough_monthly_archive_prev_next_navigation();?>
+    </div>
+    <div class="enough-next-prev-links">
+    <?php
+
+    enough_next_prev_links( 'nav-below' );
+
+
+    ?>    </div>
+    <div class="enough_prev_next_post">
+     <?php enough_prev_next_post('nav-below');?>
+    </div>
+
+
+ </div>     
+</div>
